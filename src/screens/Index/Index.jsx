5f6a48bb77c22b8bfb06bce18e42bd5c7a297146
AFelipeRiveros <133058720+AFelipeRiveros@users.noreply.github.com>
2023-11-14 19:05:51 -0500

    Initial commit

---
+++ src/screens/Index/Index.jsx
@@ -0,0 +1,125 @@
+import React from "react";
+import "./style.css";
+
+export const Index = () => {
+  return (
+    <div className="index">
+      <div className="div">
+        <div className="overlap-group">
+          <div className="frame">
+            <div className="text-wrapper">Cerrar Sesión</div>
+          </div>
+          <div className="div-wrapper">
+            <div className="text-wrapper">Presupuesto</div>
+          </div>
+          <div className="frame-2">
+            <div className="text-wrapper-2">Categorias</div>
+          </div>
+          <div className="frame-3">
+            <div className="text-wrapper-2">Ahorro</div>
+          </div>
+          <div className="frame-4">
+            <div className="text-wrapper-2">Reportes</div>
+          </div>
+          <div className="frame-5">
+            <div className="text-wrapper-2">Billetera</div>
+          </div>
+          <div className="frame-6">
+            <div className="text-wrapper">Gastos Reales</div>
+          </div>
+          <div className="frame-7">
+            <div className="text-wrapper-2">Compras</div>
+          </div>
+          <div className="frame-8">
+            <div className="text-wrapper-2">Ingresos</div>
+          </div>
+          <div className="rectangle" />
+          <img
+            className="ahorro"
+            alt="Ahorro"
+            src="https://cdn.animaapp.com/projects/655404bd2feb47a169ea5a6e/releases/65540a092feb47a169ea5a70/img/ahorro-2@2x.png"
+          />
+          <div className="text-wrapper-3">Educación Financiera</div>
+          <img
+            className="line"
+            alt="Line"
+            src="https://cdn.animaapp.com/projects/655404bd2feb47a169ea5a6e/releases/65540a092feb47a169ea5a70/img/line-2.svg"
+          />
+          <div className="text-wrapper-4">Perfil</div>
+          <div className="text-wrapper-5">Administrar</div>
+          <div className="text-wrapper-6">Ir a</div>
+        </div>
+        <div className="overlap">
+          <p className="p">SISTEMA DE APRENDIZAJE Y CONTROL DE FINANZAS PERSONALES</p>
+        </div>
+        <div className="overlap-2">
+          <div className="text-wrapper-7">Resumen</div>
+          <img
+            className="img"
+            alt="Ahorro"
+            src="https://cdn.animaapp.com/projects/655404bd2feb47a169ea5a6e/releases/65540a092feb47a169ea5a70/img/ahorro-1-1@2x.png"
+          />
+          <div className="total-ingresos">
+            Total Ingresos:
+            <br />
+            $2.500.000
+          </div>
+          <div className="total-gastos">
+            Total Gastos
+            <br />
+            $1.950.000
+          </div>
+          <div className="balance">
+            Balance:
+            <br />
+            $550.000
+          </div>
+          <div className="saldo-total">
+            Saldo Total
+            <br />
+            $530.000
+          </div>
+          <img
+            className="arrow"
+            alt="Arrow"
+            src="https://cdn.animaapp.com/projects/655404bd2feb47a169ea5a6e/releases/65540a092feb47a169ea5a70/img/arrow-1.svg"
+          />
+          <img
+            className="arrow-2"
+            alt="Arrow"
+            src="https://cdn.animaapp.com/projects/655404bd2feb47a169ea5a6e/releases/65540a092feb47a169ea5a70/img/arrow-2.svg"
+          />
+          <img
+            className="line-2"
+            alt="Line"
+            src="https://cdn.animaapp.com/projects/655404bd2feb47a169ea5a6e/releases/65540a092feb47a169ea5a70/img/line-1.svg"
+          />
+          <div className="overlap-3">
+            <div className="rectangle-2" />
+            <div className="text-wrapper-8">Movimientos</div>
+            <div className="rectangle-3" />
+            <div className="text-wrapper-9">30 Oct</div>
+            <div className="text-wrapper-10">Total Ingresos</div>
+            <div className="text-wrapper-11">$ 0.00</div>
+            <div className="text-wrapper-12">$ 2.500.000</div>
+            <div className="rectangle-4" />
+            <div className="text-wrapper-13">02 Nov</div>
+            <div className="text-wrapper-14">Servicios</div>
+            <div className="text-wrapper-15">$ 150.000</div>
+            <div className="text-wrapper-16">$ 0.00</div>
+            <div className="rectangle-5" />
+            <div className="text-wrapper-17">07 Nov</div>
+            <div className="text-wrapper-18">Apartamento</div>
+            <div className="text-wrapper-19">$ 850.000</div>
+            <div className="text-wrapper-20">$ 0.00</div>
+            <div className="rectangle-6" />
+            <div className="text-wrapper-21">15 Nov</div>
+            <div className="text-wrapper-22">Vehiculo</div>
+            <div className="text-wrapper-23">$ 630.000</div>
+            <div className="text-wrapper-24">$ 0.00</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
